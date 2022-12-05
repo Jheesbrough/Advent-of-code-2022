ca3ac62d15039c5551a2a60d762c81985b0aac07
Jonathan <jonathancheesbroughanderson@gmail.com>
2022-12-05 14:49:47 +0000

    Removed one line

--- Day5/part2.py
+++ Day5/part2.py
@@ -2,8 +2,7 @@ crates=[[],[],[],[],[],[],[],[],[]]
 with open('Day5/input.txt') as f:ls=f.read().splitlines()
 for (i,j) in [(i,j) for i in range(8) for j in range(1,10)]:
     if (ls[7-i][j*4-3])!=' ':crates[j-1].append(ls[7-i][j*4-3])
-for instruction in ls[10:]:
-    b,v,b,f,b,t = instruction.split((' '))
-    crates[int(t)-1] = crates[int(t)-1] + crates[int(f)-1][-int(v):]
-    crates[int(f)-1]=crates[int(f)-1][:-int(v)]
+for i in ls[10:]:
+    crates[int(i.split((' '))[5])-1] = crates[int(i.split((' '))[5])-1] + crates[int(i.split((' '))[3])-1][-int(i.split((' '))[1]):]
+    crates[int(i.split((' '))[3])-1]=crates[int(i.split((' '))[3])-1][:-int(i.split((' '))[1])]
 for i in range(9):print(crates[i].pop(), end='')
