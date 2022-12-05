c65f99178c816debc1f85030c50561fe6dcc3816
Jonathan <jonathancheesbroughanderson@gmail.com>
2022-12-05 14:50:41 +0000

    Shortened this by removing variable decs

--- Day5/part1.py
+++ Day5/part1.py
@@ -3,6 +3,5 @@ with open('Day5/input.txt') as f:ls=f.read().splitlines()
 for (i,j) in [(i,j) for i in range(8) for j in range(1,10)]:
     if (ls[7-i][j*4-3])!=' ':crates[j-1].append(ls[7-i][j*4-3])
 for instruction in ls[10:]:
-    b,v,b,f,b,t = instruction.split((' '))
-    for i in range(int(v)):crates[int(t)-1].append(crates[int(f)-1].pop())
+    for i in range(int(instruction.split((' '))[1])):crates[int(instruction.split((' '))[5])-1].append(crates[int(instruction.split((' '))[3])-1].pop())
 for i in range(9):print(crates[i].pop(), end='')
