15a573f44194595250fec26620a07494a6f1a2e2
Jonathan <jonathancheesbroughanderson@gmail.com>
2022-12-05 10:26:50 +0000

    Minimized

--- Day5/part2.py
+++ Day5/part2.py
@@ -1,10 +1,8 @@
 crates=[[],[],[],[],[],[],[],[],[]]
 with open('Day5/input.txt') as f:ls=f.read().splitlines()
-for j in range (1,10):
-    for i in range (8):
-        if (ls[7-i][j*4-3])!=' ':crates[j-1].append(ls[7-i][j*4-3])
-instructions = ls[10:]
-for instruction in instructions:
+for (i,j) in [(i,j) for i in range(8) for j in range(1,10)]:
+    if (ls[7-i][j*4-3])!=' ':crates[j-1].append(ls[7-i][j*4-3])
+for instruction in ls[10:]:
     b,v,b,f,b,t = instruction.split((' '))
     crates[int(t)-1] = crates[int(t)-1] + crates[int(f)-1][-int(v):]
     crates[int(f)-1]=crates[int(f)-1][:-int(v)]
